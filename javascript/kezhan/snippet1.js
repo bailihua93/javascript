6dfc2601c052b5d621cd7ee08892dcee14b55cb1
betacat <bailihua93@163.com>
2017-03-10 17:43:32 +0800

    kezhan

---
+++ javascript/kezhan/snippet1.js
@@ -0,0 +1,7 @@
+var trs = document.getElementsByTagName("tr");
+for(var tr in trs){
+    if(tr.childNodes[10].childNodes[0].nodeName == "input" ){
+        tr.childNodes[10]
+        tr.childNodes[10].childNodes[0].checked =checked;
+    }
+}
